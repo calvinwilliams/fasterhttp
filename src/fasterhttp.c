7d70fbc7c80d0aa0bdf2dc3da41b290e3e373e3f
calvinwilliams <calvinwilliams.c@gmail.com>
2016-03-22 15:55:43 +0800

    ...

--- src/fasterhttp.c
+++ src/fasterhttp.c
@@ -8,9 +8,11 @@
 struct HttpBuffer
 {
 	long		buf_size ;
-	long		len ;
 	char		*base ;
-	char		*ptr ;
+	char		*fill_ptr ;
+	long		fill_len ;
+	char		*process_ptr ;
+	long		process_len ;
 } ;
 
 struct HttpHeaderItem
@@ -23,6 +25,11 @@ struct HttpHeaderItem
 
 struct HttpHeader
 {
+	struct HttpHeaderItem	METHOD ;
+	struct HttpHeaderItem	URI ;
+	struct HttpHeaderItem	VERSION ;
+	struct HttpHeaderItem	CONTENT_LENGTH ;
+	
 	struct HttpHeaderItem	*item_array ;
 	long			item_array_size ;
 	
@@ -36,6 +43,7 @@ struct HttpEnv
 	struct HttpBuffer	request_buffer ;
 	struct HttpBuffer	response_buffer ;
 	
+	int			parse_step ;
 	struct HttpHeader	header ;
 } ;
 
@@ -61,8 +69,10 @@ struct HttpEnv *CreateHttpEnv()
 		return NULL;
 	}
 	memset( e->request_buffer.base , 0x00 , sizeof(e->request_buffer.buf_size) );
-	e->request_buffer.ptr = e->request_buffer.base ;
-	e->request_buffer.len = 0 ;
+	e->request_buffer.fill_ptr = e->request_buffer.base ;
+	e->request_buffer.process_ptr = e->request_buffer.base ;
+	e->request_buffer.fill_len = 0 ;
+	e->request_buffer.process_len = 0 ;
 	
 	e->response_buffer.buf_size = FASTERHTTP_RESPONSE_BUFSIZE_DEFAULT ;
 	e->response_buffer.base = (char*)malloc( e->response_buffer.buf_size ) ;
@@ -72,8 +82,10 @@ struct HttpEnv *CreateHttpEnv()
 		return NULL;
 	}
 	memset( e->response_buffer.base , 0x00 , sizeof(e->response_buffer.buf_size) );
-	e->response_buffer.ptr = e->response_buffer.base ;
-	e->response_buffer.len = 0 ;
+	e->response_buffer.fill_ptr = e->response_buffer.base ;
+	e->response_buffer.process_ptr = e->response_buffer.base ;
+	e->response_buffer.fill_len = 0 ;
+	e->response_buffer.process_len = 0 ;
 	
 	e->header.item_array_size = FASTERHTTP_HEADER_ITEM_ARRAYSIZE_DEFAULT ;
 	e->header.item_array = (struct HttpHeaderItem *)malloc( sizeof(struct HttpHeaderItem) * e->header.item_array_size ) ;
@@ -84,7 +96,36 @@ struct HttpEnv *CreateHttpEnv()
 	}
 	memset( e->header.item_array , 0x00 , sizeof(struct HttpHeaderItem) * e->header.item_array_size );
 	
+	e->parse_step = FASTERHTTP_PARSE_STEP_FIRSTLINE ;
+	
 	return e;
+}
+
+void ResetHttpEnv( struct HttpEnv *e )
+{
+	if( e->request_buffer.buf_size > FASTERHTTP_REQUEST_BUFSIZE_MAX )
+		ReallocHttpBuffer( &(e->request_buffer) , FASTERHTTP_REQUEST_BUFSIZE_DEFAULT );
+	if( e->response_buffer.buf_size > FASTERHTTP_RESPONSE_BUFSIZE_MAX )
+		ReallocHttpBuffer( &(e->response_buffer) , FASTERHTTP_RESPONSE_BUFSIZE_DEFAULT );
+	
+	if( e->header.item_array_size > FASTERHTTP_HEADER_ITEM_ARRAYSIZE_MAX )
+	{
+		struct HttpHeaderItem	*p = NULL ;
+		p = (struct HttpHeaderItem *)malloc( sizeof(struct HttpHeaderItem) * FASTERHTTP_HEADER_ITEM_ARRAYSIZE_DEFAULT ) ;
+		if( p )
+		{
+			memset( p , 0x00 , sizeof(struct HttpHeaderItem) * e->header.item_array_size );
+			e->header.item_array = p ;
+			e->header.item_array_size = FASTERHTTP_HEADER_ITEM_ARRAYSIZE_DEFAULT ;
+		}
+	}
+	
+	CleanHttpBuffer( &(e->request_buffer) );
+	CleanHttpBuffer( &(e->response_buffer) );
+	
+	e->parse_step = FASTERHTTP_PARSE_STEP_FIRSTLINE ;
+	
+	return;
 }
 
 void DestroyHttpEnv( struct HttpEnv *e )
@@ -130,52 +171,60 @@ char *GetHttpBufferBase( struct HttpBuffer *b )
 
 long GetHttpBufferLength( struct HttpBuffer *b )
 {
-	return b->len;
+	return b->fill_len;
 }
 
 void CleanHttpBuffer( struct HttpBuffer *b )
 {
 	memset( b->base , 0x00 , b->buf_size );
-	b->len = 0 ;
-	b->ptr = b->base ;
+	b->fill_ptr = b->base ;
+	b->fill_len = 0 ;
+	b->process_ptr = b->base ;
+	b->process_len = 0 ;
 	
 	return;
 }
 
-#define DECLARE_REALLOC_VARS \
-	long	new_buf_size ; \
-	char	*new_base = NULL ; \
-
-#define REALLOC_BUFFER(_b_) \
-	if( (_b_)->buf_size <= 100*1024*1024 ) \
-		new_buf_size = (_b_)->buf_size * 2 ; \
-	else \
-		new_buf_size = 100*1024*1024 ; \
-	new_base = (char *)realloc( (_b_)->base , new_buf_size ) ; \
-	if( new_base == NULL ) \
-		return FASTERHTTP_ERROR_ALLOC; \
-	memset( new_base + (_b_)->len , 0x00 , new_buf_size - (_b_)->len ); \
-	(_b_)->buf_size = new_buf_size ; \
-	(_b_)->ptr = (_b_)->ptr - (_b_)->base + new_base ; \
-	(_b_)->base = new_base ; \
+int ReallocHttpBuffer( struct HttpBuffer *b , long new_buf_size )
+{
+	char	*new_base = NULL ;
+	
+	if( new_buf_size == -1 )
+	{
+		if( b->buf_size <= 100*1024*1024 )
+			new_buf_size = b->buf_size * 2 ;
+		else
+			new_buf_size = 100*1024*1024 ;
+	}
+	new_base = (char *)realloc( b->base , new_buf_size ) ;
+	if( new_base == NULL )
+		return FASTERHTTP_ERROR_ALLOC;
+	memset( new_base + b->fill_len , 0x00 , new_buf_size - b->fill_len );
+	b->buf_size = new_buf_size ;
+	b->base = new_base ;
+	b->fill_ptr = b->base + b->fill_len ;
+	b->process_ptr = b->base + b->process_len ;
+	
+	return 0;
+}
 
 int StrcatfHttpBuffer( struct HttpBuffer *b , char *format , ... )
 {
 	va_list		valist ;
 	long		len ;
-	DECLARE_REALLOC_VARS
 	
 	while(1)
 	{
 		va_start( valist , format );
-		len = VSNPRINTF( b->ptr , b->buf_size-1 - b->len , format , valist ) ;
+		len = VSNPRINTF( b->fill_ptr , b->buf_size-1 - b->fill_len , format , valist ) ;
 		va_end( valist );
-		if( len == -1 || len == b->buf_size-1 - b->len )
+		if( len == -1 || len == b->buf_size-1 - b->fill_len )
 		{
-			REALLOC_BUFFER( b );
+			ReallocHttpBuffer( b , -1 );
 		}
 		else
 		{
+			b->fill_len += len ;
 			break;
 		}
 	}
@@ -185,21 +234,225 @@ int StrcatfHttpBuffer( struct HttpBuffer *b , char *format , ... )
 
 int MemcatHttpBuffer( struct HttpBuffer *b , char *base , long len )
 {
-	DECLARE_REALLOC_VARS
-	
-	while( b->len + len > b->buf_size-1 )
+	while( b->fill_len + len > b->buf_size-1 )
 	{
-		REALLOC_BUFFER( b );
+		ReallocHttpBuffer( b , -1 );
 	}
 	
-	memcpy( b->ptr , base , len );
-	b->len += len ;
+	memcpy( b->fill_ptr , base , len );
+	b->fill_len += len ;
 	
 	return 0;
 }
 
+static void AjustTimeval( struct timeval *ptv , struct timeval *t1 , struct timeval *t2 )
+{
+	ptv->tv_sec -= ( t2->tv_sec - t1->tv_sec ) ;
+	ptv->tv_usec -= ( t2->tv_usec - t1->tv_usec ) ;
+	
+	while( ptv->tv_usec < 0 )
+	{
+		ptv->tv_usec += 1000000 ;
+		ptv->tv_sec--;
+	}
+	
+	return;
+}
+
+int WriteHttpBuffer( int sock , SSL *ssl , struct HttpEnv *e , struct HttpBuffer *b )
+{
+	struct timeval	t1 , t2 ;
+	fd_set		write_fds ;
+	long		len ;
+	int		nret = 0 ;
+	
+	if( b->process_len >= b->fill_len )
+		return 0;
+	
+	gettimeofday( & t1 , NULL );
+	
+	FD_ZERO( & write_fds );
+	FD_SET( sock , & write_fds );
+	
+	nret = select( sock+1 , NULL , & write_fds , NULL , &(e->timeout) ) ;
+	if( nret == 0 )
+	{
+		return FASTERHTTP_ERROR_TCP_SELECT_SEND_TIMEOUT;
+	}
+	else if( nret != 1 )
+	{
+		return FASTERHTTP_ERROR_TCP_SELECT_SEND;
+	}
+	
+	if( ssl == NULL )
+		len = write( sock , b->process_ptr , b->fill_len-b->process_len ) ;
+	else
+		len = SSL_write( ssl , b->process_ptr , b->fill_len-b->process_len ) ;
+	if( len == -1 )
+		return FASTERHTTP_ERROR_TCP_SEND;
+	
+	gettimeofday( & t2 , NULL );
+	AjustTimeval( & (e->timeout) , & t1 , & t2 );
+	
+	b->process_ptr += len ;
+	b->process_len += len ;
+	
+	if( b->process_len >= b->fill_len )
+		return 0;
+	else
+		return FASTERHTTP_INFO_TCP_SEND_WOULDBLOCK;
+}
+
+int ReadHttpBuffer( int sock , SSL *ssl , struct HttpEnv *e , struct HttpBuffer *b )
+{
+	struct timeval	t1 , t2 ;
+	fd_set		read_fds ;
+	long		len ;
+	int		nret = 0 ;
+	
+	while( b->fill_len + FASTERHTTP_READBLOCK_SIZE_DEFAULT > b->buf_size )
+	{
+		nret = ReallocHttpBuffer( b , -1 ) ;
+		if( nret )
+			return nret;
+	}
+	
+	gettimeofday( & t1 , NULL );
+	
+	FD_ZERO( & read_fds );
+	FD_SET( sock , & read_fds );
+	
+	nret = select( sock+1 , & read_fds , NULL , NULL , &(e->timeout) ) ;
+	if( nret == 0 )
+	{
+		return FASTERHTTP_ERROR_TCP_SELECT_RECEIVE_TIMEOUT;
+	}
+	else if( nret != 1 )
+	{
+		return FASTERHTTP_ERROR_TCP_SELECT_RECEIVE;
+	}
+	
+	if( ssl == NULL )
+		len = (long)read( sock , b->fill_ptr , FASTERHTTP_READBLOCK_SIZE_DEFAULT ) ;
+	else
+		len = (long)SSL_read( ssl , b->fill_ptr , FASTERHTTP_READBLOCK_SIZE_DEFAULT ) ;
+	if( len == -1 )
+		return FASTERHTTP_ERROR_TCP_RECEIVE;
+	else if( len == 0 )
+		return FASTERHTTP_ERROR_HTTP_TRUNCATION;
+	
+	gettimeofday( & t2 , NULL );
+	AjustTimeval( & (e->timeout) , & t1 , & t2 );
+	
+	b->fill_ptr += len ;
+	b->fill_len += len ;
+	
+	return 0;
+}
+
+static int ParseHttpRequestHeaderFirstLine( struct HttpEnv *e , struct HttpBuffer *b )
+{
+	char	*line_end = NULL ;
+	
+	line_end = strstr( e->line_begin , "\r\n" ) ;
+	if( line_end )
+	{
+		char	*p = NULL ;
+		for( p = e->line_begin ; (*p) == ' ' ; p++ )
+			;
+		if( (*p) == '\r' )
+		e->header.METHOD.value_ptr = p ;
+		for( p = e->line_begin ; (*p) != ' ' ; p++ )
+			;
+		e->header.METHOD.value_len = p - e->header.METHOD.value_ptr ;
+	}
+	
+	
+	
+	
+	
+}
+
+static int ParseHttpResponseHeaderFirstLine( struct HttpEnv *e , struct HttpBuffer *b )
+{
+}
+
+int ParseHttpBuffer( struct HttpEnv *e , struct HttpBuffer *b )
+{
+	int		nret = 0 ;
+	
+	while(1)
+	{
+		switch( e->parse_step )
+		{
+			case FASTERHTTP_PARSE_STEP_FIRSTLINE :
+				
+				e->line_begin = b->base ;
+				
+				if( b == &(e->request_buffer) )
+				{
+					nret = ParseHttpRequestHeaderFirstLine( e , b ) ;
+					if( nret == FASTERHTTP_INFO_NEED_MORE_HTTP_BUFFER )
+						break;
+					else if( nret )
+						return nret;
+					else
+						e->parse_step = 
+				}
+				else if( b == &(e->response_buffer)  )
+				{
+					nret = ParseHttpResponseHeaderFirstLine( e , b ) ;
+					if( nret == FASTERHTTP_INFO_NEED_MORE_HTTP_BUFFER )
+						break;
+					else if( nret )
+						return nret;
+				}
+				else
+				{
+					return FASTERHTTP_ERROR_PARAMTER;
+				}
+				
+				break;
+			
+			case FASTERHTTP_PARSE_STEP_HEADER :
+				
+				
+				
+				break;
+			
+			case FASTERHTTP_PARSE_STEP_BODY :
+				
+				
+				
+				break;
+			
+			case FASTERHTTP_PARSE_STEP_DONE :
+				
+				
+				
+				break;
+			
+			default :
+				return FASTERHTTP_ERROR_INTERNAL;
+		}
+	}
+	
+	return nret;
+}
+
 int SendHttpRequest( int sock , SSL *ssl , struct HttpEnv *e )
 {
+	int		nret = 0 ;
+	
+	while(1)
+	{
+		nret = WriteHttpBuffer( sock , ssl , e , &(e->request_buffer) ) ;
+		if( nret == 0 )
+			break;
+		else if( nret != FASTERHTTP_INFO_TCP_SEND_WOULDBLOCK )
+			return nret;
+	}
+	
 	return 0;
 }
 
@@ -215,16 +468,47 @@ int ParseHttpResponse( struct HttpEnv *e )
 
 int ReceiveHttpRequest( int sock , SSL *ssl , struct HttpEnv *e )
 {
+	int		nret = 0 ;
+	
+	while(1)
+	{
+		nret = ReadHttpBuffer( sock , ssl , e , &(e->request_buffer) ) ;
+		if( nret )
+			return nret;
+		
+		
+		
+		
+	}
+	
 	return 0;
 }
 
 int ParseHttpRequest( struct HttpEnv *e )
 {
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 }
 
 int SendHttpResponse( int sock , SSL *ssl , struct HttpEnv *e )
 {
+	int		nret = 0 ;
+	
+	while(1)
+	{
+		nret = WriteHttpBuffer( sock , ssl , e , &(e->response_buffer) ) ;
+		if( nret == 0 )
+			break;
+		else if( nret != FASTERHTTP_INFO_TCP_SEND_WOULDBLOCK )
+			return nret;
+	}
+	
 	return 0;
 }
 
--- src/fasterhttp.h
+++ src/fasterhttp.h
@@ -63,18 +63,20 @@ extern "C" {
 #endif
 
 #define FASTERHTTP_ERROR_ALLOC				-11
+#define FASTERHTTP_ERROR_PARAMTER			-12
+#define FASTERHTTP_ERROR_INTERNAL			-14
+#define FASTERHTTP_INFO_TCP_RECEIVE_WOULDBLOCK		30
 #define FASTERHTTP_ERROR_TCP_SELECT_RECEIVE		-31
 #define FASTERHTTP_ERROR_TCP_SELECT_RECEIVE_TIMEOUT	-32
 #define FASTERHTTP_ERROR_TCP_RECEIVE			-33
+#define FASTERHTTP_INFO_TCP_SEND_WOULDBLOCK		40
 #define FASTERHTTP_ERROR_TCP_SELECT_SEND		-41
 #define FASTERHTTP_ERROR_TCP_SELECT_SEND_TIMEOUT	-42
 #define FASTERHTTP_ERROR_TCP_SEND			-43
 #define FASTERHTTP_ERROR_FD_SELECT_READ			-51
 #define FASTERHTTP_ERROR_FD_SELECT_READ_TIMEOUT		-52
 #define FASTERHTTP_ERROR_FD_READ			-53
-#define FASTERHTTP_INFO_NEED_MORE_HTTP_FIRSTLINE	100
-#define FASTERHTTP_INFO_NEED_MORE_HTTP_HEADER		101
-#define FASTERHTTP_INFO_NEED_MORE_HTTP_BODY		102
+#define FASTERHTTP_INFO_NEED_MORE_HTTP_BUFFER		100
 #define FASTERHTTP_ERROR_HTTP_TRUNCATION		-101
 #define FASTERHTTP_ERROR_HTTP_HEADERLINE_INVALID	-102
 #define FASTERHTTP_ERROR_HTTP_PROCESSING		-104
@@ -86,10 +88,19 @@ extern "C" {
 #define FASTERHTTP_ERROR_URI_TOOLONG			-414
 #define FASTERHTTP_ERROR_VERSION_NOT_SUPPORTED		-505
 
-#define FASTERHTTP_REQUEST_BUFSIZE_DEFAULT		16*1024
-#define FASTERHTTP_RESPONSE_BUFSIZE_DEFAULT		16*1024
+#define FASTERHTTP_REQUEST_BUFSIZE_DEFAULT		400*1024
+#define FASTERHTTP_RESPONSE_BUFSIZE_DEFAULT		400*1024
 #define FASTERHTTP_READBLOCK_SIZE_DEFAULT		4*1024
 #define FASTERHTTP_HEADER_ITEM_ARRAYSIZE_DEFAULT	64
+
+#define FASTERHTTP_REQUEST_BUFSIZE_MAX			10*1024*1024
+#define FASTERHTTP_RESPONSE_BUFSIZE_MAX			10*1024*1024
+#define FASTERHTTP_HEADER_ITEM_ARRAYSIZE_MAX		256
+
+#define FASTERHTTP_PARSE_STEP_FIRSTLINE			0
+#define FASTERHTTP_PARSE_STEP_HEADER			1
+#define FASTERHTTP_PARSE_STEP_BODY			2
+#define FASTERHTTP_PARSE_STEP_DONE			3
 
 #define HTTP_METHOD_POST				"POST"
 #define HTTP_METHOD_GET					"GET"
@@ -98,6 +109,7 @@ struct HttpBuffer ;
 struct HttpEnv ;
 
 _WINDLL_FUNC struct HttpEnv *CreateHttpEnv();
+_WINDLL_FUNC void ResetHttpEnv( struct HttpEnv *e );
 _WINDLL_FUNC void DestroyHttpEnv( struct HttpEnv *e );
 
 _WINDLL_FUNC void SetHttpTimeout( struct HttpEnv *e , long timeout );
@@ -107,8 +119,11 @@ _WINDLL_FUNC struct HttpBuffer *GetHttpResponseBuffer( struct HttpEnv *e );
 _WINDLL_FUNC char *GetHttpBufferBase( struct HttpBuffer *b );
 _WINDLL_FUNC long GetHttpBufferLength( struct HttpBuffer *b );
 _WINDLL_FUNC void CleanHttpBuffer( struct HttpBuffer *b );
+_WINDLL_FUNC int ReallocHttpBuffer( struct HttpBuffer *b , long new_buf_size );
 _WINDLL_FUNC int StrcatfHttpBuffer( struct HttpBuffer *b , char *format , ... );
 _WINDLL_FUNC int MemcatHttpBuffer( struct HttpBuffer *b , char *base , long len );
+_WINDLL_FUNC int WriteHttpBuffer( int sock , SSL *ssl , struct HttpEnv *e , struct HttpBuffer *b );
+_WINDLL_FUNC int ReadHttpBuffer( int sock , SSL *ssl , struct HttpEnv *e , struct HttpBuffer *b );
 
 _WINDLL_FUNC int SendHttpRequest( int sock , SSL *ssl , struct HttpEnv *e );
 _WINDLL_FUNC int ReceiveHttpResponse( int sock , SSL *ssl , struct HttpEnv *e );
@@ -117,8 +132,6 @@ _WINDLL_FUNC int ParseHttpResponse( struct HttpEnv *e );
 _WINDLL_FUNC int ReceiveHttpRequest( int sock , SSL *ssl , struct HttpEnv *e );
 _WINDLL_FUNC int ParseHttpRequest( struct HttpEnv *e );
 _WINDLL_FUNC int SendHttpResponse( int sock , SSL *ssl , struct HttpEnv *e );
-
-
 
 #endif
 
